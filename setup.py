b9bb230fabfb93381d4f2cc82aeadcdd67959d9d
chandrabrt <chandra2khadka4@gmail.com>
2017-09-10 12:53:29 +0545

    new

--- setup.py
+++ setup.py
@@ -3,24 +3,24 @@ try:
 except ImportError:
     from distutils.core import setup
 
-requirements = [pkg.split('=')[0] for pkg in open('requirements.txt').readlines()]
+
 
 
 classifiers = ['Environment :: Console',
                'Programming Language :: Python :: 3'
                ]
 
+#requirements = [pkg.split('=')[0] for pkg in open('requirements.txt').readlines()]
 
 setup(
     name='keep-password',
-
-    version='1.0',  # Ideally should be same as your GitHub release tag varsion
+    version='1.6',  # Ideally should be same as your GitHub release tag varsion
     description='Save password for username',
     author='chandra khadka',
     author_email='chandra2khadka4@gmail.com',
     url='https://github.com/chandrabrt/keep',
     scripts=['src/keep-password'],
-    install_requires=requirements,
+    install_requires=['pyperclip'],
     package=['keep_password'],
     package_dir={'keep_password': 'src/keep_password'},
     classifiers=classifiers
