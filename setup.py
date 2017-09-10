c0c3c38260c35dbb31d7a31fe9f65c72e4a8b30f
chandrabrt <chandra2khadka4@gmail.com>
2017-09-10 12:21:22 +0545

    new version

--- setup.py
+++ setup.py
@@ -12,17 +12,17 @@ classifiers = ['Environment :: Console',
 
 
 setup(
-    name='extract',
+    name='keep-password',
 
     version='1.0',  # Ideally should be same as your GitHub release tag varsion
     description='Save password for username',
     author='chandra khadka',
     author_email='chandra2khadka4@gmail.com',
     url='https://github.com/chandrabrt/keep',
-    scripts=['src/ex-tract'],
+    scripts=['src/keep-password'],
     install_requires=requirements,
-    package=['ex_tract'],
-    package_dir={'ex_tract': 'src/ex_tract'},
+    package=['keep_password'],
+    package_dir={'keep_password': 'src/keep_password'},
     classifiers=classifiers
 )
 
